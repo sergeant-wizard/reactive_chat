b10ba9a33cc6fecf9291f3e75f98daff543cf4f3
Ryo Miyajima <r.miyajima@aktsk.jp>
2015-10-06 18:35:53 +0900

    hello world from react

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var React = require("react");
+
+var ChatMessage = require("./components/ChatMessage.react.js");
+
+React.render(
+  <ChatMessage />,
+  document.getElementById("reactive-chat-app")
+);
---
+++ js/components/ChatMessage.react.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var ChatMessage = React.createClass({
+
+  render: function() {
+    return (
+      <div>
+        Aeosutnh
+      </div>
+    );
+  }
+});
+
+module.exports = ChatMessage;
