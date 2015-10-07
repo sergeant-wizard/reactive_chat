bc986cd929034840fd6f930d94eff9b845644f1b
Ryo Miyajima <r.miyajima@aktsk.jp>
2015-10-07 18:04:02 +0900

    use prototypes to distinguish left/right rows

--- js/components/ChatMessage.react.js
+++ js/components/ChatMessage.react.js
@@ -1,55 +1,98 @@
 "use strict";
 
 var React = require('react/addons');
+var ReactTransitionGroup = React.addons.TransitionGroup;
 var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
 
-function getChatMessageClass(isBot) {
-  var chatMessageClass = "well";
-  if (isBot) {
-    chatMessageClass += " leftMessage";
-  } else {
-    chatMessageClass += " rightMessage";
-  }
-  return chatMessageClass;
-}
-
-function getMessageRow(text, isBot) {
-  // note that the key attribute is required for ReactCSSTransitionGroup
-  var mainMessageCol = (
-    <div className="col-md-9 col-xs-9">
-      <ReactCSSTransitionGroup transitionName="fadeIn" transitionAppear={true}>
-        <div key="1" className={getChatMessageClass(isBot)}>
-          {text}
-        </div>
-      </ReactCSSTransitionGroup>
-    </div>
-  );
-  var messageSpacerCol = (
+function messageRow(params) {
+  this.initialCSS = params.initialCSS;
+  this.animatedCSS = params.animatedCSS;
+  this.render = params.render;
+  this.getContent = params.getContent;
+  this.messageSpacerCol = (
     <div className="col-md-3 col-xs-3">
     </div>
   );
-  if (isBot) {
+  this.getMessageCol = function(text) {
     return (
-      <div className="row">
-        {mainMessageCol}
-        {messageSpacerCol}
+      <div className="col-md-9 col-xs-9">
+        <div className="well">
+          {text}
+        </div>
       </div>
     );
-  } else {
+  };
+  this.getComponent = function() {
+    var messageRow = this;
+    return React.createClass({
+      componentWillAppear: function(callback) {
+        var DOMNode = $(this.getDOMNode());
+        DOMNode.css(messageRow.initialCSS);
+        DOMNode.animate(
+          messageRow.animatedCSS,
+          250
+        );
+        callback();
+      },
+      componentDidAppear: function() {
+        console.log("did appear");
+      },
+      render: function() {
+        return messageRow.getContent(this.props.text);
+      }
+    });
+  };
+}
+
+var leftMessageRow = new messageRow({
+  initialCSS: {
+    "opacity": 0,
+    "left": "-100px",
+    "position": "relative"
+  },
+  animatedCSS: {
+    "opacity": 1,
+    "left": "0px",
+  },
+  getContent: function(text) {
     return (
       <div className="row">
-        {messageSpacerCol}
-        {mainMessageCol}
+        {this.getMessageCol(text)}
+        {this.messageSpacerCol}
       </div>
     );
   }
-}
+});
+
+var rightMessageRow = new messageRow({
+  initialCSS: {
+    "opacity": 0,
+    "left": "100px",
+    "position": "relative"
+  },
+  animatedCSS: {
+    "opacity": 1,
+    "left": "0px"
+  },
+  getContent: function(text) {
+    return (
+      <div className="row">
+        {this.messageSpacerCol}
+        {this.getMessageCol(text)}
+      </div>
+    );
+  }
+});
 
 var ChatMessage = React.createClass({
   render: function() {
+    var messageRow = this.props.isBot === true ? leftMessageRow : rightMessageRow;
+    var MessageComponent = messageRow.getComponent();
     return (
       <div className="container">
-        {getMessageRow(this.props.text, this.props.isBot)}
+        <ReactTransitionGroup transitionName="fadeIn" transitionAppear={true}>
+          <MessageComponent text={this.props.text} />
+        </ReactTransitionGroup>
       </div>
     );
   }
